f8534259cd8544be656917d2616301f661dd443c
mahendar <mahendarchillara464@gmail.com>
2024-12-24 15:27:22 +0530

    EDA and Problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,11 +1,11 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format()
-    file_name,exc_tb.tb_lineno,str(error)
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+    file_name,exc_tb.tb_lineno,str(error))
 
     return error_message
 
@@ -19,11 +19,11 @@ class CustomException(Exception):
     
 
 
-# if __name__=="__main__":
-#     try:
-#         a=1/0
-#     except Exception as e:
-#         logging.info("Divide by zero")
-#         raise CustomException(e,sys)
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise CustomException(e,sys)
 
 
